523661fc58bca87ed823318008674b90877a8382
rafaverde <rafaverde@msn.com>
2024-09-30 15:32:38 -0300

    Post page layout created.

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,9 +1,71 @@
-import { PostContainer } from "./styles"
+import {
+  ArrowSquareOut,
+  Calendar,
+  CaretLeft,
+  ChatCircle,
+  GithubLogo,
+} from "@phosphor-icons/react"
+import { PostContainer, PostContent, PostHeader } from "./styles"
+import { NavLink } from "react-router-dom"
 
 export function Post() {
   return (
     <PostContainer>
-      <h1>Post</h1>
+      <PostHeader>
+        <nav>
+          <ul>
+            <NavLink to={"/"}>
+              <li>
+                <CaretLeft size={16} />
+                Voltar
+              </li>
+            </NavLink>
+            <li>
+              Ver no Github <ArrowSquareOut size={16} weight="bold" />
+            </li>
+          </ul>
+        </nav>
+
+        <h1>JavaScript data types and data structures</h1>
+
+        <ul>
+          <li>
+            <GithubLogo size={18} weight="fill" />
+            rafaverde
+          </li>
+          <li>
+            <Calendar size={18} weight="fill" />
+            Há 1 dia
+          </li>
+          <li>
+            <ChatCircle size={18} weight="fill" />2
+          </li>
+        </ul>
+      </PostHeader>
+
+      <PostContent>
+        <p>
+          Programming languages all have built-in data structures, but these
+          often differ from one language to another. This article attempts to
+          list the built-in data structures available in JavaScript and what
+          properties they have. These can be used to build other data
+          structures. Wherever possible, comparisons with other languages are
+          drawn.
+        </p>
+        <p>
+          Dynamic typing JavaScript is a loosely typed and dynamic language.
+          Variables in JavaScript are not directly associated with any
+          particular value type, and any variable can be assigned (and
+          re-assigned) values of all types:
+        </p>
+        <div>
+          <code lang="javascript">
+            let foo = 42; // foo is now a number <br />
+            foo = ‘bar’; // foo is now a string <br />
+            foo = true; // foo is now a boolean
+          </code>
+        </div>
+      </PostContent>
     </PostContainer>
   )
 }
--- src/pages/Post/styles.ts
+++ src/pages/Post/styles.ts
@@ -5,3 +5,63 @@ export const PostContainer = styled.div`
   max-width: 864px;
   margin: 0 auto;
 `
+
+export const PostHeader = styled.header`
+  padding: 2rem;
+  background-color: ${({ theme }) => theme.colors["base-profile"]};
+
+  nav {
+    margin-bottom: 1.25rem;
+
+    ul {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      list-style: none;
+
+      li {
+        display: flex;
+        align-items: center;
+        gap: 5px;
+
+        font-size: 0.75rem;
+        text-transform: uppercase;
+
+        color: ${({ theme }) => theme.colors.primary};
+        border-bottom: 1px solid transparent;
+        cursor: pointer;
+
+        &:hover {
+          border-bottom: 1px solid ${({ theme }) => theme.colors.primary};
+        }
+      }
+    }
+  }
+
+  ul {
+    display: flex;
+    align-items: center;
+    gap: 2rem;
+    margin-top: 1rem;
+
+    list-style: none;
+
+    li {
+      display: flex;
+      align-items: center;
+      gap: 5px;
+
+      color: ${({ theme }) => theme.colors["base-span"]};
+    }
+  }
+`
+
+export const PostContent = styled.article`
+  padding: 2.5rem 2rem;
+
+  div {
+    padding: 2rem;
+    margin-top: 1.5rem;
+    background-color: ${({ theme }) => theme.colors["base-post"]};
+  }
+`
